d0187afc88ec8ca0dbd582a93cff0dd99cc0cdbd
suama <umaisuamaumai@icloud.com>
2020-11-23 08:08:10 +0000

    add colum status

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_16_110530) do
+ActiveRecord::Schema.define(version: 2020_11_23_072019) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status", limit: 10
   end
 
 end
