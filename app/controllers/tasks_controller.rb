03230aaa726d6749bb1a2642d19e9c904116de30
suama <umaisuamaumai@icloud.com>
2021-01-04 07:36:11 +0000

    細かい修正

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,6 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
   before_action :correct_user, only: [:show, :update, :edit, :destroy]
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
-
   
   def index
       @tasks = current_user.tasks.order(id: :desc).page(params[:page])
@@ -23,7 +21,7 @@ class TasksController < ApplicationController
     else
     @tasks = current_user.tasks.order(id: :desc).page(params[:page])
     flash.now[:danger] = 'Task投稿失敗'
-    render 'tasks/index'
+    render :new
     end
   end
 
@@ -51,10 +49,6 @@ class TasksController < ApplicationController
   end
   
   private
-  
-  def set_task
-    @task = Task.find(params[:id])
-  end
   
   def task_params
     params.require(:task).permit(:content, :status)
