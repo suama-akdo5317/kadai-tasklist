b21d8eab3f263994b443e03d9d3266c6291adb9b
suama <umaisuamaumai@icloud.com>
2021-01-04 08:19:05 +0000

    CRUDのうちURを削除

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   
   get 'signup', to: 'users#new'
 
-  resources :tasks
+  resources :tasks, only: [:index, :new, :create]
 
   resources :users, only: [:index, :show, :new, :create]
 
